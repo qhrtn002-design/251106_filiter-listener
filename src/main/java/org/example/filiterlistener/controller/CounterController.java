7535b0f219025dd772d10abf518df747368508d8
qhrtn002-design <qhrtn002@gmail.com>
2025-11-06 10:37:04 +0900

    step1

--- src/main/java/org/example/filiterlistener/controller/CounterController.java
+++ src/main/java/org/example/filiterlistener/controller/CounterController.java
@@ -1,4 +1,54 @@
-package org.example.filiterlistener.controller;
+package org.example.filterlistener.controller;
 
-public class CounterController {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+
+@WebServlet("/")
+public class CounterController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 접속했을 때 세션과 쿠키에 카운트를 늘려주는...
+        // 세션으로 카운트를 늘리기
+        HttpSession session = req.getSession(); // true -> 없으면 새로 만들겠다
+        countBySession(session);
+        countByCookie(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/counter.jsp").forward(req, resp);
+    }
+
+    private void countBySession(HttpSession session) {
+        Object counterValue = session.getAttribute("counter");
+        int counter = 0;
+        if (counterValue != null) {
+            counter = (Integer) session.getAttribute("counter"); // 변환이 안되면 에러
+        }
+        counter++;
+        session.setAttribute("counter", counter);
+    }
+
+    private void countByCookie(HttpServletRequest request, HttpServletResponse response) {
+        // Request 내부에 쿠키가 있는 체크
+        Cookie[] cookies = request.getCookies();
+        int counter = 0;
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("counter")) {
+                counter = Integer.parseInt(cookie.getValue());
+                break; // 반복문 종료
+            }
+        }
+        counter++;
+        // forward 방식이라면... attribute에 해당 값을 넣는...
+        request.setAttribute("counter", counter);
+        // Response에다가 addCookie로 덮어씌우기를 함
+        Cookie newCookie = new Cookie("counter", Integer.toString(counter));
+        newCookie.setPath("/");
+        newCookie.setSecure(true);
+        newCookie.setMaxAge(60 * 60 * 24); // 86400
+        newCookie.setHttpOnly(true);
+        response.addCookie(newCookie); // 바꿔놓은 쿠키가 반영되는 시점? -> resp.
+        // forward -> counter.jsp -> cookie - req.
+        // 한 박자씩 늦은 결과물을 받게 됨
+    }
 }
--- src/main/webapp/WEB-INF/views/counter.jsp
+++ src/main/webapp/WEB-INF/views/counter.jsp
@@ -1,16 +1,24 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: 서희수
-  Date: 25. 11. 6.
-  Time: 오전 9:25
-  To change this template use File | Settings | File Templates.
---%>
+<%-- JSP를 방문 시 자동으로 세션이 만들어지는 걸 방지 --%>
+<%-- <%@ page session="false" %> --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>카운터</title>
 </head>
 <body>
-
+<p>접속을 카운트합니다</p>
+<%-- session -> request에 담아주기 때문에 이렇게 꺼내서 쓸 수 있음 --%>
+<p>세션 : <%= (Integer) session.getAttribute("counter") %></p>
+<%--    <%--%>
+<%--        int cookieCounter = 0;--%>
+<%--        for (Cookie c : request.getCookies()) {--%>
+<%--            if(c.getName().equals("counter")) {--%>
+<%--                cookieCounter = Integer.parseInt(c.getValue());--%>
+<%--                break;--%>
+<%--            }--%>
+<%--        }--%>
+<%--    %>--%>
+<%--    <p>쿠키 : <%= cookieCounter %></p>--%>
+<p>쿠키 : <%= request.getAttribute("counter") %></p>
 </body>
 </html>
